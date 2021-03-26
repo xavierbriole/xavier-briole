4f66b64050e35e27a2b5100bd3f0e3783a91239e
xavierbriole <xavierbriole@users.noreply.github.com>
2021-03-26 13:05:41 +0000

    deploy: f20c6939e4b1e5dd386b852840ff03cadc6861a9

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 /**
- * Dark Theme
- */
-
-const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
-const siteTheme = localStorage.getItem('theme');
-const setDark = (e) => {
-    document.body.className = (e.matches ? 'dark-theme' : '');
-}
-
-if (siteTheme === null) {
-    mediaQueryList.addListener(setDark);
-    document.addEventListener('DOMContentLoaded', function() {
-        setDark(mediaQueryList);
-    });
-}
-
-/**
  * Progress Bar
  */
 
@@ -45,7 +28,7 @@ cookiebanner.type = 'application/javascript';
 cookiebanner.id = 'cookiebanner';
 cookiebanner.src = 'https://www.xavierbriole.com/js/cookiebanner.min.js';
 cookiebanner.dataset.message = 'I use cookies to improve your browsing experience.';
-cookiebanner.dataset.moreinfo = 'https://cookie-consent.app.cloudstinger.com/why-websites-use-cookies/';
+cookiebanner.dataset.moreinfo = 'https://aboutcookies.org/';
 cookiebanner.async = true;
 document.body.appendChild(cookiebanner);
 
@@ -53,20 +36,17 @@ document.body.appendChild(cookiebanner);
  * Chatbot
  */
 
-var chatbot = document.createElement('script');
-chatbot.src = 'https://bots.xavierbriole.com/assets/modules/channel-web/inject.js';
-document.body.appendChild(chatbot);
+/*
+var chatbotScript = document.createElement('script');
+chatbotScript.src = 'https://bots.xavierbriole.com/assets/modules/channel-web/inject.js';
+document.body.appendChild(chatbotScript);
 
 window.onload = function() {
     var chatbotInit = document.createElement('script');
     chatbotInit.type = 'text/javascript';
-    var code = 'window.botpressWebChat.init({ host: "https://bots.xavierbriole.com", botId: "jarvis", botName: "Jarvis", botConvoDescription: "Xavier\'s personal assistant", showConversationsButton: false, enableReset: false, enableTranscriptDownload: false, extraStylesheet: "' + window.location.origin + '/css/jarvis-dark.min.css" }); window.addEventListener("message", function(event) { if (event.data.name === "webchatOpened") { window.botpressWebChat.sendEvent({ type: "open-chat" }) } if (event.data.name === "webchatClosed") { window.botpressWebChat.sendEvent({ type: "close-chat" }) } })';
-
-    try {
-        chatbotInit.appendChild(document.createTextNode(code));
-        document.body.appendChild(chatbotInit);
-    } catch (e) {
-        s.text = code;
-        document.body.appendChild(chatbotInit);
-    }
+    
+    var chatbotConfig = 'window.botpressWebChat.init({ host: "https://bots.xavierbriole.com", botId: "jarvis", botName: "Jarvis", botConvoDescription: "Xavier\'s personal assistant", showConversationsButton: false, enableReset: false, enableTranscriptDownload: false, extraStylesheet: "' + window.location.origin + '/css/jarvis-dark.min.css" }); window.addEventListener("message", function(event) { if (event.data.name === "webchatOpened") { window.botpressWebChat.sendEvent({ type: "open-chat" }) } if (event.data.name === "webchatClosed") { window.botpressWebChat.sendEvent({ type: "close-chat" }) } })';
+    chatbotInit.appendChild(document.createTextNode(chatbotConfig));
+    document.body.appendChild(chatbotInit);
 }
+*/
