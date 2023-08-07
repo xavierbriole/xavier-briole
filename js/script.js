ba95de5fb6c895970e63d564d9bb5d003e9f9027
xavierbriole <xavierbriole@users.noreply.github.com>
2023-08-07 12:56:13 +0000

    deploy: 03571ec3d6396f36bc2e562ea6b2d9876ad27bf1

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@
  */
 
 if (window.location.href.includes("/posts/") && window.location.href !== window.location.origin + "/posts/") {
-    var progressContainer = document.createElement('div');
+    const progressContainer = document.createElement('div');
     progressContainer.className = 'progress-container';
     document.querySelector('.header').before(progressContainer);
 
-    var progressBar = document.createElement('div');
+    const progressBar = document.createElement('div');
     progressBar.className = 'progress-bar';
     document.querySelector('.progress-container').appendChild(progressBar);
 
     window.onscroll = function() {
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        var scrolled = (winScroll / height) * 100;
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrolled = (winScroll / height) * 100;
         document.querySelector('.progress-bar').style.width = scrolled + "%";
     }
 }
@@ -23,7 +23,7 @@ if (window.location.href.includes("/posts/") && window.location.href !== window.
  * Cookie Banner
  */
 
-var cookiebanner = document.createElement('script');
+const cookiebanner = document.createElement('script');
 cookiebanner.type = 'application/javascript';
 cookiebanner.id = 'cookiebanner';
 cookiebanner.src = 'https://www.xavierbriole.com/js/cookiebanner.min.js';
@@ -31,3 +31,21 @@ cookiebanner.dataset.message = 'I use cookies to improve your browsing experienc
 cookiebanner.dataset.moreinfo = 'https://aboutcookies.org/';
 cookiebanner.async = true;
 document.body.appendChild(cookiebanner);
+
+/**
+ * Update meta theme-color when dark mode is enabled
+ */
+
+const LIGHT_MODE_COLOR = "#fafafa";
+const DARK_MODE_COLOR = "#1b1c1d";
+const matchQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+window.onload = function() {
+    const isDarkMode = matchQuery.matches;
+    document.querySelector('meta[name="theme-color"]').setAttribute("content", isDarkMode ? DARK_MODE_COLOR : LIGHT_MODE_COLOR);
+}
+
+matchQuery.addEventListener("change", function(e) {
+    const isDarkMode = e.matches;
+    document.querySelector('meta[name="theme-color"]').setAttribute("content", isDarkMode ? DARK_MODE_COLOR : LIGHT_MODE_COLOR);
+});
